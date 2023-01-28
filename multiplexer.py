b13eb2bf99a017d8a65833e6549f782e85d6c4d1
wadayamada <wada0421514@gmail.com>
2023-01-29 02:58:36 +0900

    4bitで括った

--- multiplexer.py
+++ multiplexer.py
@@ -12,11 +12,11 @@ def multiplexer(A, B, S):
 # S1,S0でA,B,C,Dから選択
 
 
-def multiplexer4(A, B, C, D, S1, S0):
+def multiplexer4(A, B, C, D, S):
     return multiplexer(
-        multiplexer(A, B, S0),
-        multiplexer(C, D, S0),
-        S1
+        multiplexer(A, B, S[1]),
+        multiplexer(C, D, S[1]),
+        S[0]
     )
 
 # S3,S2,S1,S0でi0000~i1111から選択
@@ -25,22 +25,52 @@ def multiplexer4(A, B, C, D, S1, S0):
 def multiplexer16(
         i0000, i0001, i0010, i0011, i0100, i0101, i0110, i0111,
         i1000, i1001, i1010, i1011, i1100, i1101, i1110, i1111,
-        S3, S2, S1, S0):
+        S):
     return multiplexer4(
         multiplexer4(
-            i0000, i0001, i0010, i0011, S1, S0
+            i0000, i0001, i0010, i0011, S[2:4]
         ),
         multiplexer4(
-            i0100, i0101, i0110, i0111, S1, S0
+            i0100, i0101, i0110, i0111, S[2:4]
         ),
         multiplexer4(
-            i1000, i1001, i1010, i1011, S1, S0
+            i1000, i1001, i1010, i1011, S[2:4]
         ),
         multiplexer4(
-            i1100, i1101, i1110, i1111, S1, S0
+            i1100, i1101, i1110, i1111, S[2:4]
         ),
-        S3, S2
+        S[0:2]
     )
+
+
+def multiplexer_4bit(A, B, S):
+    return [multiplexer(A[i], B[i], S) for i in range(4)]
+
+
+def multiplexer16_4bit(
+        i0000, i0001, i0010, i0011, i0100, i0101, i0110, i0111,
+        i1000, i1001, i1010, i1011, i1100, i1101, i1110, i1111,
+        S):
+    return [multiplexer16(
+        i0000[i], i0001[i], i0010[i], i0011[i],
+        i0100[i], i0101[i], i0110[i], i0111[i],
+        i1000[i], i1001[i], i1010[i], i1011[i],
+        i1100[i], i1101[i], i1110[i], i1111[i],
+        S
+    ) for i in range(4)]
+
+
+def multiplexer16_8bit(
+        i0000, i0001, i0010, i0011, i0100, i0101, i0110, i0111,
+        i1000, i1001, i1010, i1011, i1100, i1101, i1110, i1111,
+        S):
+    return [multiplexer16(
+        i0000[i], i0001[i], i0010[i], i0011[i],
+        i0100[i], i0101[i], i0110[i], i0111[i],
+        i1000[i], i1001[i], i1010[i], i1011[i],
+        i1100[i], i1101[i], i1110[i], i1111[i],
+        S
+    ) for i in range(8)]
 
 
 if __name__ == "__main__":
@@ -48,11 +78,11 @@ if __name__ == "__main__":
           multiplexer(1, 0, 0), multiplexer(0, 1, 1))
 
     print("multiplexer4:",
-          multiplexer4(1, 0, 0, 0, 0, 0), multiplexer4(0, 1, 0, 0, 0, 1),
-          multiplexer4(0, 0, 1, 0, 1, 0), multiplexer4(0, 0, 0, 1, 1, 1))
+          multiplexer4(1, 0, 0, 0, [0, 0]), multiplexer4(0, 1, 0, 0, [0, 1]),
+          multiplexer4(0, 0, 1, 0, [1, 0]), multiplexer4(0, 0, 0, 1, [1, 1]))
 
     print("multiplxer16:",
           multiplexer16(
               0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-              0, 1, 0, 0)
+              [0, 1, 0, 0])
           )
--- td4.py
+++ td4.py
@@ -1,6 +1,6 @@
-from dff import DFF
+from dff import DFF, DFF4bit
 from rom import ROM
-from multiplexer import multiplexer16, multiplexer
+from multiplexer import multiplexer16, multiplexer, multiplexer16_4bit, multiplexer_4bit
 
 
 # とりあえず動作する4bitのコンピューター
@@ -13,86 +13,85 @@ class TD4():
         # 出力LED
         self.LED = [0, 0, 0, 0]
         # 汎用レジスタ
-        self.register_a = [DFF(), DFF(), DFF(), DFF()]
-        self.register_b = [DFF(), DFF(), DFF(), DFF()]
+        self.register_a = DFF4bit()
+        self.register_b = DFF4bit()
         # キャリーフラグ(CF)
         self.register_cf = DFF()
         # 命令ポインタ(IP)
-        self.register_ip = [DFF(), DFF(), DFF(), DFF()]
+        self.register_ip = DFF4bit()
         # IOレジスタ
-        self.register_out = [DFF(), DFF(), DFF(), DFF()]
+        self.register_out = DFF4bit()
         # ROM
         self.rom = ROM()
 
     def clock(self):
         # 新しい値をregisterに読み込む
-        for i in range(4):
-            self.register_a[i].clock()
-            self.register_b[i].clock()
-            self.register_ip[i].clock()
-            self.register_out[i].clock()
+        self.register_a.clock()
+        self.register_b.clock()
+        self.register_ip.clock()
+        self.register_out.clock()
         self.register_cf.clock()
         # 出力
-        self.LED = [dff.Q for dff in self.register_out]
+        self.LED = self.register_out.get_Q()
         # ROMからデータを取り出す
-        data = self.rom.get_data([dff.Q for dff in self.register_ip])
+        data = self.rom.get_data(self.register_ip.get_Q())
         opecode = data[0:4]
         imm = data[4:8]
         # ALUで計算
         next_a, next_b, next_cf, next_ip, next_out = self.ALU(opecode, imm)
-        for i in range(4):
-            self.register_a[i].input(next_a[i])
-            self.register_b[i].input(next_b[i])
-            self.register_ip[i].input(next_ip[i])
-            self.register_out[i].input(next_out[i])
-        self.register_cf.input(next_cf)
+        self.register_a.set(next_a)
+        self.register_b.set(next_b)
+        self.register_ip.set(next_ip)
+        self.register_out.set(next_out)
+        self.register_cf.set(next_cf)
 
     def ALU(self, opecode, imm):
         # 汎用レジスタAの計算
-        register_a_Q = [i.Q for i in self.register_a]
+        register_a_Q = self.register_a.get_Q()
         cf_add_A_imm, result_add_A_imm = self.Add(register_a_Q, imm)
-        register_b_Q = [i.Q for i in self.register_b]
-        next_a = [
-            multiplexer16(
-                result_add_A_imm[i], register_b_Q[i], self.switch[i], imm[i],
-                register_a_Q[i], register_a_Q[i], register_a_Q[i], register_a_Q[i],
-                register_a_Q[i], register_a_Q[i], register_a_Q[i], register_a_Q[i],
-                register_a_Q[i], register_a_Q[i], register_a_Q[i], register_a_Q[i],
-                opecode[0], opecode[1], opecode[2], opecode[3]) for i in range(4)]
+        register_b_Q = self.register_b.get_Q()
+        next_a = multiplexer16_4bit(
+            result_add_A_imm, register_b_Q, self.switch, imm,
+            register_a_Q, register_a_Q, register_a_Q, register_a_Q,
+            register_a_Q, register_a_Q, register_a_Q, register_a_Q,
+            register_a_Q, register_a_Q, register_a_Q, register_a_Q,
+            opecode
+        )
         # 汎用レジスタBの計算
         cf_add_B_imm, result_add_B_imm = self.Add(register_b_Q, imm)
-        next_b = [
-            multiplexer16(register_b_Q[i], register_b_Q[i], register_b_Q[i], register_b_Q[i],
-                          register_a_Q[i], result_add_B_imm[i], self.switch[i], imm[i],
-                          register_b_Q[i], register_b_Q[i], register_b_Q[i], register_b_Q[i],
-                          register_b_Q[i], register_b_Q[i], register_b_Q[i], register_b_Q[i],
-                          opecode[0], opecode[1], opecode[2], opecode[3]
-                          ) for i in range(4)
-        ]
+        next_b = multiplexer16_4bit(
+            register_b_Q, register_b_Q, register_b_Q, register_b_Q,
+            register_a_Q, result_add_B_imm, self.switch, imm,
+            register_b_Q, register_b_Q, register_b_Q, register_b_Q,
+            register_b_Q, register_b_Q, register_b_Q, register_b_Q,
+            opecode
+        )
         # キャリーフラグ(CF)の計算
         next_cf = multiplexer16(
-            cf_add_A_imm, 0, 0, 0, 0, cf_add_B_imm, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0,
-            opecode[0], opecode[1], opecode[2], opecode[3])
+            cf_add_A_imm, 0, 0, 0,
+            0, cf_add_B_imm, 0, 0,
+            0, 0, 0, 0,
+            0, 0, 0, 0,
+            opecode
+        )
         # 命令ポインタ(IP)の計算
-        ip_plus1_cf, ip_plus1_result = self.Add([i.Q for i in self.register_ip], [0, 0, 0, 1])
-        next_ip = [multiplexer16(
-            ip_plus1_result[i], ip_plus1_result[i], ip_plus1_result[i], ip_plus1_result[i],
-            ip_plus1_result[i], ip_plus1_result[i], ip_plus1_result[i], ip_plus1_result[i],
-            ip_plus1_result[i], ip_plus1_result[i], ip_plus1_result[i], ip_plus1_result[i],
-            ip_plus1_result[i], ip_plus1_result[i], multiplexer(imm[i], ip_plus1_result[i], self.register_cf.Q), imm[i],
-            opecode[0], opecode[1], opecode[2], opecode[3]) for i in range(4)
-        ]
+        ip_plus1_cf, ip_plus1_result = self.Add(self.register_ip.get_Q(), [0, 0, 0, 1])
+        next_ip = multiplexer16_4bit(
+            ip_plus1_result, ip_plus1_result, ip_plus1_result, ip_plus1_result,
+            ip_plus1_result, ip_plus1_result, ip_plus1_result, ip_plus1_result,
+            ip_plus1_result, ip_plus1_result, ip_plus1_result, ip_plus1_result,
+            ip_plus1_result, ip_plus1_result, multiplexer_4bit(imm, ip_plus1_result, self.register_cf.Q), imm,
+            opecode
+        )
         # アウトプットの計算
-        next_out = [
-            multiplexer16(
-                0, 0, 0, 0, 0, 0, 0, 0,
-                0, register_b_Q[i], 0, imm[i], 0, 0, 0, 0,
-                opecode[0], opecode[1], opecode[2], opecode[3]
-            )
-            for i in range(4)
-        ]
-
+        next_out = multiplexer16_4bit(
+            [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0],
+            [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0],
+            [0, 0, 0, 0], register_b_Q, [0, 0, 0, 0], imm,
+            [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0],
+            opecode
+        )
+        # print
         print("CF         :", next_cf)
         print("register_A :", next_a)
         print("register_B :", next_b)
